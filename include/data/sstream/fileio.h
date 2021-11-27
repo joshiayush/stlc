3beb5ef79211117f8a8bc29f037f27f76964499e
Ayush Joshi <ayush854032@gmail.com>
2021-11-27 16:39:22 +0530

    refactor(header_gaurd): prefix header gaurd with `_CJSON_` to make it more `cjson` oriented

--- include/data/sstream/fileio.h
+++ include/data/sstream/fileio.h
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU General Public License along with
 // this program. If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef _DATA_SSTREAM_FILEIO_H_
-#define _DATA_SSTREAM_FILEIO_H_
+#ifndef _CJSON_DATA_SSTREAM_FILEIO_H_
+#define _CJSON_DATA_SSTREAM_FILEIO_H_
 
 #include <stdio.h>
 #include <sys/types.h>
