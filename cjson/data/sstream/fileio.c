dae18cc347d2c425f14e993d71d5a8be0450bfa8
Ayush Joshi <ayush854032@gmail.com>
2021-11-27 15:36:20 +0530

    feat(fileio): add function `stringstream_readfile()` to read the content of the given file in the `stringstream` instance
    add function `stringstream_readfile()` to read the content of the given file in the
    `stringstream` instance

---
+++ cjson/data/sstream/fileio.c
@@ -0,0 +1,45 @@
+// Copyright (C) 2021  Ayush Joshi
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include "data/sstream/fileio.h"
+
+#include <stdio.h>
+#include <sys/types.h>
+
+#include "data/sstream/modifiers.h"
+#include "data/sstream/sstream.h"
+
+/**
+ * @brief Reads the content of the file given in the stringstream instance.
+ *
+ * @param[in] sstream stringstream instance.
+ * @param[in] file File pointer.
+ * @param[in] length Length of the data to be read. Reads the entire file if the
+ * length given is 0.
+ */
+void stringstream_readfile(stringstream* const sstream, FILE* const file,
+                           size_t length) {
+  if (length == 0) {
+    size_t orig_cur_pos = ftell(file);
+    fseek(file, 0L, SEEK_END);
+    length = ftell(file) - orig_cur_pos;
+    fseek(file, orig_cur_pos, SEEK_SET);
+  }
+  stringstream_realloc(sstream, sstream->length + length);
+  fread(sstream->data + sstream->length, sizeof(char), length, file);
+  sstream->length += length;
+  _terminate(*sstream);
+}
---
+++ include/data/sstream/fileio.h
@@ -0,0 +1,36 @@
+// Copyright (C) 2021  Ayush Joshi
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef _DATA_SSTREAM_FILEIO_H_
+#define _DATA_SSTREAM_FILEIO_H_
+
+#include <stdio.h>
+#include <sys/types.h>
+
+#include "data/sstream/sstream.h"
+
+/**
+ * @brief Reads the content of the file given in the stringstream instance.
+ *
+ * @param[in] sstream stringstream instance.
+ * @param[in] file File pointer.
+ * @param[in] length Length of the data to be read. Reads the entire file if the
+ * length given is 0.
+ */
+void stringstream_readfile(stringstream* const sstream, FILE* const file,
+                           size_t length);
+
+#endif
