09db612effb6ea8e0cc0643ac9dfbcde53714166
Ayush Joshi <ayush854032@gmail.com>
2021-11-28 13:44:10 +0530

    feat(bool): use macro `TRUE` and `FALSE` for boolean values and `bool_t` for boolean types
    * use macro `TRUE` and `FALSE` defined in header `bool.h` to represent boolean values
    * use type `bool_t` to define boolean types
    
    `bool_t` is a typedef for `unsigned char` which is `1` bytes of data which is enough for
    storing boolean values

---
+++ include/bool.h
@@ -0,0 +1,27 @@
+// Copyright (C) 2021  Ayush Joshi
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef _CJSON_BOOL_H_
+#define _CJSON_BOOL_H_
+
+#include <sys/types.h>
+
+#define TRUE 1
+#define FALSE !TRUE
+
+typedef __uint8_t bool_t;
+
+#endif
