61b4de1b5a70c00f609f473af116e8eed5493821
Ayush Joshi <ayush854032@gmail.com>
2021-11-28 13:57:58 +0530

    feat(accessors): use function `d_array_set()` and `d_array_get()` to set and get the values of a `d_array` instance respectively
    * use function `d_array_set()` to set the value of a `d_array` instance at a specific
      index position provided that the index value given is in bounds
    
    * use function `d_array_get()` to get a `const void*` to the value sitting at the given
      index of the `d_array` instance where again provided that the index value is in bounds

---
+++ cjson/data/d_array/accessors.c
@@ -0,0 +1,46 @@
+// Copyright (C) 2021  Ayush Joshi
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include "data/d_array/accessors.h"
+
+#include <stdio.h>
+#include <sys/types.h>
+
+/**
+ * @brief Sets the value in a @a d_array instance provided that the @p idx value
+ * given is in bounds.
+ *
+ * @param[in] darray @a d_array instance.
+ * @param[in] elem Element to add or to replace the @a d_array data with.
+ * @param[in] idx Index where to add the @p elem given.
+ */
+void d_array_set(d_array* const darray, const void* const elem,
+                 const size_t idx) {
+  if (idx > darray->capacity)
+    return;
+  darray->data[idx] = elem;
+}
+
+/**
+ * @brief Returns the @a d_array instance data at given @p idx.
+ *
+ * @param[in] darray @a d_array instance.
+ * @param[in] idx Element index.
+ * @return const void* to the data located at the location.
+ */
+const void* d_array_get(const d_array* const darray, const size_t idx) {
+  return idx > darray->size ? NULL : darray->data[idx];
+}
