e548b9aee60fa45d4f6ed2ce52688d3f8a5fa269
Ayush Joshi <ayush854032@gmail.com>
2021-11-29 16:59:31 +0530

    tests(sstream): test `stringstream_alloc()` macro with `0` arguments
    * test macro `stringstream_alloc()` with `0` number of arguments to test if the
      `_stringstream_alloc_args` struct instance is evaluated to
      `{.length = SSTREAM_DEFAULT_SIZE}` or not
    
    * test `sstream` `length` instance
    
    * test `sstream` `capacity` instance

---
+++ tests/sstream/test_sstream.h
@@ -0,0 +1,38 @@
+// Copyright (C) 2021  Ayush Joshi
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.
+//
+// See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+
+#include "data/sstream/accessors.h"
+#include "data/sstream/sstream.h"
+
+class SstreamTest : public ::testing::Test {
+ protected:
+  void TearDown() override { stringstream_dealloc(&sstream); }
+
+ protected:
+  stringstream sstream;
+};
+
+TEST_F(SstreamTest, SstreamTest_testStringStreamAllocMacroWith0Arguments_Test) {
+  sstream = stringstream_alloc();
+  ASSERT_NE(sstream.data, (void*)0);
+  ASSERT_EQ(sstream.length, SSTREAM_DEFAULT_SIZE);
+  uint32_t capacity = 1;
+  while (capacity < sstream.length)
+    capacity <<= 1;
+  ASSERT_EQ(sstream.capacity, capacity);
+}
