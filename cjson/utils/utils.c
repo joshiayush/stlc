25e4db7e7e0718c125018ab9414ed86c1407f673
Ayush Joshi <ayush854032@gmail.com>
2022-04-06 17:43:48 +0530

    Document utility function `SplitStr()`
    `SplitStr()` function is one of the most complicated function in our `utils` library so
    we must document each and every operation we are performing over there.
    
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- cjson/utils/utils.c
+++ cjson/utils/utils.c
@@ -47,39 +47,78 @@
 // and the returned array of pointers too.
 char** SplitStr(const char* str, const char* sep) {
   size_t ncomps = 0;
+
+  // Length of the given separator string (it can be a single letter string or a
+  // multiple letter string).
   size_t seplen = strlen(sep);
   size_t strlen_ = strlen(str);
+
+  // Compute number of total components we have in the given path string i.e.,
+  // `str`.
   for (size_t i = 0; i < strlen_; ++i) {
+    // If we find a separator string at the current position in the given path
+    // then increment `ncomps`.
     if ((strncmp((str + i), sep, seplen) == 0))
       ++ncomps;
   }
+
+  // If the given path string have a separator at the end that means we've
+  // computed one more component then we should so we must decrement `ncomps`.
   if (strncmp((str + strlen_ - seplen), sep, seplen) == 0)
     --ncomps;
+
+  // Allocate `ncomps` block of memory, each of a size of `char*` to store the
+  // splited components.
   char** comps = (char**)calloc(ncomps, sizeof(char*));
+
+  // Declare `beg` and `end` pointers to point to the beginning and the end of a
+  // component in the given path string.
   const char* beg = NULL;
   const char* end = NULL;
+
+  // Number of component in the given path string that we are taking out as a
+  // component for our `comps` pointer.
   size_t comp_pos = 0;
+
   for (size_t i = 0; i < strlen_; ++i) {
+    // Because `comp_pos` starts with 0, we must break the for loop once the
+    // `comp_pos` is equals to `ncomps`.
     if (ncomps == comp_pos)
       break;
+
+    // If `end` pointer is not pointing to a `NULL` value then our new beginning
+    // must start from `end + seplen`.
     if (end)
       beg = (end++ + 1);
     else
-      beg = (str + i);
+      beg =
+          (str + i);  // Otherwise, our new beginning must start from `str + i`.
+
+    // Our `beg` pointer is set now find the position of the `end` pointer.
     for (size_t j = 0; j < strlen_; ++j) {
       if (end && (strncmp(end, sep, seplen) == 0))
         break;
       end = (beg + j);
     }
+
+    // Continue if we couldn't find a valid `end` pointer.  `end` must always be
+    // at a greater position than `beg`.
     if ((end - beg) <= 0)
       continue;
+
+    // Allocate memory for the current component because we are about to take it
+    // out.
     char* str_ = (char*)malloc((end - beg) * sizeof(char));
     char* ptr = str_;
     for (beg; beg != end; ++beg)
-      *ptr++ = *beg;
-    *ptr = '\0';
+      *ptr++ = *beg;  // Copy every element from `beg` to `end` to the newly
+                      // allocated memory.
+    *ptr = '\0';      // At the end append a `end-of-string` character.
+
+    // Save the address of the dynamic memory where our current component lies.
     comps[comp_pos++] = str_;
   }
+
   return comps;
 }
 
@@ -200,6 +239,7 @@ char* NormPath(char* const path) {
   const char* dotdot = "..";
   const char* sep = "/";
 
+  // Path becomes dot (.) in case the given `path` is empty.
   if (strcmp(path, empty) == 0) {
     strcpy(path, dot);
     return path;
