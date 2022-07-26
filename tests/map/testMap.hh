ff5436606d828c045cd8a0427a06d994d5370023
Ayush Joshi <ayush854032@gmail.com>
2022-07-26 13:30:14 +0530

    Test `MapEntry` instance returned by function `MapAllocEntriesWithHash()` when given a `key`, `value` and a `hash
    Test `MapEntry` linked list when a node is allocated using different values.
    
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- tests/map/testMap.hh
+++ tests/map/testMap.hh
@@ -37,6 +37,64 @@
 #include "data/map/map.h"
 #include "data/map/ops.h"
 
+TEST(MapAllocEntryWithHashTest, TestWhenA16DigitsHashIsUsed) {
+  char key[5];
+  std::strcpy(key, "key1");
+  char value[7];
+  std::strcpy(value, "value1");
+  MapEntry* mapentry = MapAllocEntryWithHash(key, value, 0xbabe7cee878d3e62);
+  ASSERT_NE(mapentry, nullptr);
+  EXPECT_STREQ((const char*)mapentry->key, key);
+  EXPECT_STREQ((const char*)mapentry->value, value);
+  EXPECT_EQ(mapentry->hash, 0xbabe7cee878d3e62);
+  EXPECT_EQ(mapentry->next, nullptr);
+  delete mapentry;
+}
+
+TEST(MapAllocEntryWithHashTest, TestWhenMultipleEntriesAreLinkedTogether) {
+  char key1[5], key2[5], key3[5];
+  std::strcpy(key1, "key1");
+  std::strcpy(key2, "key2");
+  std::strcpy(key3, "key3");
+
+  char value1[7], value2[7], value3[7];
+  std::strcpy(value1, "value1");
+  std::strcpy(value2, "value2");
+  std::strcpy(value3, "value3");
+
+  MapEntry* mapentry1 = MapAllocEntryWithHash(key1, value1, 0xbabe7cee878d3e62);
+  MapEntry* mapentry2 = MapAllocEntryWithHash(key2, value2, 0x280f93cdf26cf053);
+  MapEntry* mapentry3 = MapAllocEntryWithHash(key3, value3, 0xd53239e006c95f4a);
+
+  ASSERT_NE(mapentry1, nullptr);
+  ASSERT_NE(mapentry2, nullptr);
+  ASSERT_NE(mapentry3, nullptr);
+
+  mapentry1->next = mapentry2;
+  mapentry2->next = mapentry3;
+
+  EXPECT_STREQ((const char*)mapentry1->key, key1);
+  EXPECT_STREQ((const char*)mapentry1->value, value1);
+
+  EXPECT_STREQ((const char*)mapentry2->key, key2);
+  EXPECT_STREQ((const char*)mapentry2->value, value2);
+
+  EXPECT_STREQ((const char*)mapentry3->key, key3);
+  EXPECT_STREQ((const char*)mapentry3->value, value3);
+
+  EXPECT_EQ(mapentry1->hash, 0xbabe7cee878d3e62);
+  EXPECT_EQ(mapentry2->hash, 0x280f93cdf26cf053);
+  EXPECT_EQ(mapentry3->hash, 0xd53239e006c95f4a);
+
+  EXPECT_EQ(mapentry1->next, mapentry2);
+  EXPECT_EQ(mapentry2->next, mapentry3);
+  EXPECT_EQ(mapentry3->next, nullptr);
+
+  delete mapentry1;
+  delete mapentry2;
+  delete mapentry3;
+}
+
 class MapTest : public testing::Test {
  protected:
   void TearDown() override { MapFree(&map); }
@@ -45,7 +103,7 @@ class MapTest : public testing::Test {
   Map map;
 };
 
-TEST_F(MapTest, MapInstanceTestWhenAllocatedWithMapAllocStrAsKey) {
+TEST_F(MapTest, TestWhenAllocatedWithMapAllocStrAsKey) {
   map = MapAllocStrAsKey();
 
   char mohika[7];
