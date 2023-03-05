0f0dabdbd47564869e6356d6a7669fb0acb0349f
joshiayush <ayush854032@gmail.com>
2023-03-05 14:58:32 +0530

    Add googletest unit tests for `StringStreamReadFile` and `StringStreamWriteFile`
    "StringStreamReadFile" was tested to ensure that it reads the correct length
    of data from the file provided, and if length is 0, it reads the entire file
    correctly.
    
    "StringStreamWriteFile" was tested to ensure that it writes data from
    "StringStream" to the file provided correctly, with various test cases to
    verify the different aspects of the function's behavior. The test suite writes
    data to "StringStream" and then writes it to a temporary file using
    "StringStreamWriteFile", then reads that file to compare the written data with
    the original data.
    
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

--- tests/sstream/testFileIO.hh
+++ tests/sstream/testFileIO.hh
@@ -37,85 +37,43 @@
 
 #include "sstream/sstream.h"
 
-class StringStreamFileIOTest : public ::testing::Test {
- protected:
-  void TearDown() override {
-    if (sstream.data != NULL)
-      StringStreamDealloc(&sstream);
-  }
-
- protected:
-  StringStream sstream;
-};
-
-// clang-format off
-const char* kFileCopyRightText =
-    "// Copyright 2021, The cjson authors. All rights reserved.\n"
-    "//\n"
-    "// Redistribution and use in source and binary forms, with or without\n"
-    "// modification, are permitted provided that the following conditions are\n"
-    "// met:\n"
-    "//\n"
-    "//     * Redistributions of source code must retain the above copyright\n"
-    "// notice, this list of conditions and the following disclaimer.\n"
-    "//     * Redistributions in binary form must reproduce the above\n"
-    "// copyright notice, this list of conditions and the following disclaimer\n"
-    "// in the documentation and/or other materials provided with the\n"
-    "// distribution.\n"
-    "//     * Neither the name of The cjson authors. nor the names of its\n"
-    "// contributors may be used to endorse or promote products derived from\n"
-    "// this software without specific prior written permission.\n"
-    "//\n"
-    "// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS\n"
-    "// \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT\n"
-    "// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR\n"
-    "// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT\n"
-    "// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,\n"
-    "// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT\n"
-    "// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,\n"
-    "// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY\n"
-    "// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n"
-    "// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n"
-    "// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n";
-// clang-format on
-
-class StringStreamReadFileTest : public StringStreamFileIOTest {};
-
-TEST_F(StringStreamReadFileTest, WhenStringStreamInstanceIsNull) {
-  std::FILE* file = std::fopen(__FILE__, "r");
-  StringStreamReadFile(NULL, file, 0);
+TEST(StringStreamReadFile, StringStreamReadFileTest) {
+  StringStream sstream = StringStreamAlloc();
+  FILE* file = std::fopen("test.txt", "w");
+  ASSERT_NE(file, nullptr);
+  const char* content = "This is a test file.\n";
+  std::fputs(content, file);
   std::fclose(file);
-  ASSERT_EQ(sstream.length, 0);
-  ASSERT_EQ(sstream.capacity, 0);
-  ASSERT_EQ(sstream.data, nullptr);
+
+  file = std::fopen("test.txt", "r");
+  ASSERT_NE(file, nullptr);
+
+  StringStreamReadFile(&sstream, file, std::strlen(content));
+  std::fclose(file);
+  EXPECT_EQ(sstream.length, std::strlen(content));
+  EXPECT_STREQ(sstream.data, content);
+
+  std::remove("test.txt");
+  StringStreamDealloc(&sstream);
 }
 
-TEST_F(StringStreamReadFileTest, WhenFILEInstanceIsNull) {
-  StringStreamReadFile(&sstream, NULL, 0);
-  ASSERT_EQ(sstream.length, 0);
-  ASSERT_EQ(sstream.capacity, 0);
-  ASSERT_EQ(sstream.data, nullptr);
-}
-
-TEST_F(StringStreamReadFileTest,
-       WhenTestedTheFileCopyRightTextIsReadCorrectly) {
-  std::FILE* file = std::fopen(__FILE__, "r");
-  StringStreamReadFile(&sstream, file, std::strlen(kFileCopyRightText));
+TEST(StringStreamWriteFile, StringStreamWriteFileTest) {
+  StringStream sstream = StringStreamAlloc();
+  StringStreamConcat(&sstream, "This is a test string.");
+  FILE* file = std::fopen("test.txt", "w");
+  ASSERT_NE(file, nullptr);
+  StringStreamWriteFile(&sstream, file, 5, 10);
   std::fclose(file);
-  EXPECT_STREQ(sstream.data, kFileCopyRightText);
-}
 
-TEST_F(StringStreamReadFileTest, WhenTestedIfTheCurrentFileIsReadCorrectly) {
-  std::FILE* file = std::fopen(__FILE__, "r");
-  StringStreamReadFile(&sstream, file, 0);
-  ASSERT_NE(sstream.length, 0);
-  ASSERT_NE(sstream.capacity, 0);
-  ASSERT_NE(sstream.data, nullptr);
-  std::fseek(file, 0L, SEEK_SET);
-  char* current_file_content = new char[sstream.length];
-  std::fread(current_file_content, sizeof(char), sstream.length, file);
+  file = std::fopen("test.txt", "r");
+  ASSERT_NE(file, nullptr);
+  char buffer[50];
+  std::fgets(buffer, 50, file);
   std::fclose(file);
-  ASSERT_STREQ(sstream.data, current_file_content);
+  EXPECT_STREQ(buffer, "is a ");
+
+  std::remove("test.txt");
+  StringStreamDealloc(&sstream);
 }
 
 #endif  // CJSON_TESTS_SSTREAM_TESTFILEIO_HH_
